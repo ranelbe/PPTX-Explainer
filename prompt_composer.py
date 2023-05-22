af055dfb7181c68d6e0b78ca51bd0e3ecc4e47da
ranelbe <ranelbe@edu.hac.ac.il>
2023-05-22 10:05:30 +0300

    add prompt composer feature

---
+++ prompt_composer.py
@@ -0,0 +1,26 @@
+
+PROMPT_SUFFIX = """provide a detailed explanation for this presentation slide, 
+you may include the following: 
+main message, key points, related concepts, supporting evidence, implications, 
+and any additional examples or illustrations to help understand the content better.
+"""
+
+
+class PromptComposer:
+    """ Composes a prompt suffix for each slide. """
+
+    def __init__(self, slides_text):
+        """ initialization.
+            :param slides_text: list of slides text.
+        """
+        self.slides_text = slides_text
+
+    def compose(self):
+        """
+        Composes a prompt suffix for each slide.
+        :return: list of composed slides text.
+        """
+        composed_slides_text = list(
+            map(lambda slide_text: f"{slide_text}{PROMPT_SUFFIX}", self.slides_text)
+        )
+        return composed_slides_text
